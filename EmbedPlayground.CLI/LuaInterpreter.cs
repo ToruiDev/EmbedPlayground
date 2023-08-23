4a7ee6d80881d88ec4567b8bd2dcd6ece76c229a
Florian Rinke <florian@torui.de>
2023-08-23 21:24:08 +0200

    initial commit

---
+++ EmbedPlayground.CLI/LuaInterpreter.cs
@@ -0,0 +1,98 @@
+﻿using NLua;
+using NLua.Event;
+using NLua.Exceptions;
+
+namespace EmbedPlayground.CLI;
+
+public class LuaInterpreter : IDisposable
+{
+    public string Name { get; set; } = $"anonymous@{DateTime.Now}";
+    public string Source { get; set; } = String.Empty;
+    public bool IsRunning { get; } = false;
+    private Lua _state;
+    private CancellationToken? _currentToken;
+    
+    public LuaInterpreter()
+    {
+        _state = new Lua();
+        _state.DebugHook += StateOnDebugHook;
+        
+        
+        var baseScript = File.ReadAllText("./Sandbox.lua");
+        _state["log"] = this.Debug;
+        //_state.RegisterFunction("log", typeof(LuaInterpreter).GetMethod(nameof(Debug)));
+        var sandbox = _state.DoString(baseScript, "BaseScript");
+    }
+
+    private void StateOnDebugHook(object? sender, DebugHookEventArgs e)
+    {
+        if (_currentToken is not { IsCancellationRequested: true }) return;
+        
+        var l = sender as Lua;
+        l?.State?.Error("Execution cancelled");
+    }
+
+    public Task<(object? ok, object? result)> ExecuteAsync(CancellationToken token = default)
+    {
+        _currentToken = token;
+        return Task.Run(Execute, token);
+    }
+
+    private (object? ok, object? result) Execute()
+    {
+        return ExecuteSandboxedWithErrorHandling(this.Source, this.Name);
+    }
+
+    private (object? ok, object? result) ExecuteSandboxedWithErrorHandling(string code, string name)
+    {
+        lock (_state)
+        {
+            try
+            {
+                _state["__code"] = code;
+
+                #if DUMP_VARS
+                var ex = _state.DoString("print(__code)", "debug_print");
+                var globals = _state.DoString("""
+for k,v in pairs(_G) do
+    print("Global key", k, "value", v)
+end
+""", "globals");
+                
+                var locals = _state.DoString("""
+local i = 0
+repeat
+    local k, v = debug.getlocal(1, i)
+    if k then
+        print(k, v)
+        i = i + 1
+    end
+until nil == k
+""", "locals");
+                #endif
+                var quota = false;
+                var env = "log = log";
+                var tmp = _state.DoString($"local ok, result = pcall(sandbox.run, __code, {{quota={quota}, env = {{ {env} }}}})", Name);
+                var (ok, result) = (_state["ok"], _state["result"]);
+                return (ok, result);
+            }
+            catch (LuaScriptException lse)
+            {
+                Console.Error.WriteLine(lse);
+            }
+        }
+
+        return (null, null);
+    }
+
+    public void Debug(string pattern, params object[] args)
+    {
+        Console.Error.WriteLine($"{Name}:" + pattern, args);
+    }
+    
+    public void Dispose()
+    {
+        _state.Dispose();
+    }
+}
+
---
+++ EmbedPlayground.CLI/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EmbedPlayground.CLI;
+using KeraLua;
+using Lua = NLua.Lua;
+using LuaFunction = NLua.LuaFunction;
+
+var interpreter = new LuaInterpreter()
+{
+	Source = """
+		for i=0,10,1
+		do
+			log('hello world ')
+		end 
+	"""
+};
+var res = await interpreter.ExecuteAsync();
+Console.WriteLine(res);
+return;
+
+Console.WriteLine("Hello, World!");
+
+	
+using Lua state = new Lua ();
+
+state["x"] = 27;
+state.DoString(@"y = 10 + x*(5 + 2)");
+
+var ev = new EventDict();
+state["events"] = ev;
+
+state.SetDebugHook(LuaHookMask.Line, 0);
+state.DebugHook += (sender, eventArgs) =>
+{
+	if (eventArgs.LuaDebug.CurrentLine < 1) return;
+	
+	var l = sender as Lua;
+	l?.State?.Error("Execution manually aborted");
+};
+
+state.DoString("""
+	function ScriptFunc (val1)
+		events:Debug(val1)
+	end
+""");
+
+state.DoString("events:On('init', ScriptFunc)");
+
+
+
+ev.events["init"].Call("hello world");
+
+Console.WriteLine("MathResult: " + state["y"]);
+
+class EventDict
+{
+	public Dictionary<string, LuaFunction> events = new();
+	
+	public void On(string eventName, LuaFunction func)
+	{
+		events[eventName] = func;
+	}
+
+	public void Debug(string s)
+	{
+		Console.WriteLine("Lua:>" + s);
+	}
+}
